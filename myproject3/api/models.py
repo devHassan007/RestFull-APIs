3ba85061f9648f418bf9ff112b841ee9c83f6de9
devHassan007 <thisishassanmalik007@gmail.com>
2026-07-29 16:00:46 +0500

    APIViews

--- myproject3/api/models.py
+++ myproject3/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=30)
     age = models.IntegerField()
-    city = models.CharField(max_length=50)
+    city = models.CharField(max_length=90)
 
     def __str__(self):
         return self.name
